289ae25c38f4f8c119b13b8d74f658283cf83fac
Adam Gardner <adga2012@gmail.com>
2024-12-30 12:40:48 -0700

    fix regex, add confirmation modal and spinner, cleanup

--- DuplicateFileManager.Service/Services/FileManagerService.cs
+++ DuplicateFileManager.Service/Services/FileManagerService.cs
@@ -7,7 +7,7 @@ namespace DuplicateFileManager.Service.Services;
 
 public class FileManagerService(IFileService fileService) : IFileManagerService
 {
-    private readonly Regex _duplicatedFileNameRegex = new(@"([\s-]+[cC]opy)+(?=\.[a-zA-Z0-9]+)", RegexOptions.Compiled);
+    private readonly Regex _duplicatedFileNameRegex = new(@"([\s-]+[cC]opy[\(\)\s0-9]*)+(?=\.[a-zA-Z0-9]+)", RegexOptions.Compiled);
 
     public async Task DeleteFiles(DeleteFilesRequest request, CancellationToken cancellationToken)
     {
@@ -37,7 +37,7 @@ public class FileManagerService(IFileService fileService) : IFileManagerService
         return fileDictionary.GroupBy(x => x.FileHash)
             .Where(x => x.Count() > 1)
             .Select(x => new DuplicateFileData(
-                Path.GetFileName(x.MaxBy(y => _duplicatedFileNameRegex.Replace(y.FilePath, string.Empty))!.FilePath),
+                Path.GetFileName(x.MaxBy(y => _duplicatedFileNameRegex.Replace(y.FilePath, string.Empty, Int32.MaxValue))!.FilePath),
                 x.Select(y => y.FilePath).OrderBy(y => y.Length).ToList()))
             .OrderBy(x => x.CommonFileName)
             .ThenBy(x => x.FilePaths.Count)
